0b0f6baa2a6d1c2c35742d4d58c4fd6c803f9e18
mirekZhou <mirek.zhou@gmail.com>
2017-11-30 14:53:30 +0800

    ~~

--- src/components/common/detailParts/myLuckyBoard.vue
+++ src/components/common/detailParts/myLuckyBoard.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="my-lucky-board">
-		<div class="title">我的夺宝数据</div>
+		<div class="title">我的幸运码：{{luckyNumber}}</div>
 
 		<div class="content">
 			<div class="left-part">
@@ -11,14 +11,21 @@
 								<div class="td td1">助攻好友</div>
 								<div class="td td2">日期</div>
 								<div class="td td3">夺宝助攻获得号码</div>
+								<div class="clear"></div>
 							</div>
 						</div>
 
 						<div class="tbody">
-							<div class="tr">
-								<div class="td td1"></div>
-								<div class="td td2"></div>
-								<div class="td td3"></div>
+							<div class="tr" v-for="item in leftList">
+								<div class="td td1">
+									<span class="head-icon"></span>
+									<span class="text">{{item.phone}}</span>
+								</div>
+								<div class="td td2">{{item.date}}</div>
+								<div class="td td3">
+									<span>{{item.number}}</span>
+								</div>
+								<div class="clear"></div>
 							</div>
 						</div>
 					</div>
@@ -27,6 +34,30 @@
 
 			<div class="right-part">
 				<div class="wrapper">
+					<div class="table">
+						<div class="thead">
+							<div class="tr">
+								<div class="td td1">助攻好友</div>
+								<div class="td td2">日期</div>
+								<div class="td td3">夺宝助攻获得号码</div>
+								<div class="clear"></div>
+							</div>
+						</div>
+
+						<div class="tbody">
+							<div class="tr" v-for="item in rightList">
+								<div class="td td1">
+									<span class="head-icon"></span>
+									<span class="text">{{item.phone}}</span>
+								</div>
+								<div class="td td2">{{item.date}}</div>
+								<div class="td td3">
+									<span>{{item.number}}</span>
+								</div>
+								<div class="clear"></div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
@@ -44,7 +75,23 @@
 
 		data: function () {
 			return {
-				luckyNumber: '10101010'
+				luckyNumber: '10101010',
+
+				leftList: [
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'}
+				],
+
+				rightList: [
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'},
+					{phone: '139****1234', date: '2017-09-31 17:30:23', number: '156489'}
+				]
 			}
 		},
 
@@ -82,20 +129,6 @@
 					height: 100%;
 					padding-right: 18px;
 					width: 100%;
-
-					.table {
-						.thead {
-
-						}
-
-						.tbody {
-
-						}
-
-						.tr {
-							width: 100%;
-						}
-					}
 				}
 			}
 
@@ -109,6 +142,57 @@
 					height: 100%;
 					padding-right: 18px;
 					width: 100%;
+				}
+			}
+
+			.table {
+				.tr {
+					width: 100%;
+
+					.td {
+						height: 46px;
+						line-height: 46px;
+						float: left;
+						text-align: center;
+					}
+
+					.td1 {
+						width: 180px;
+
+						.head-icon {
+							background-image: url("../../../assets/header.png");
+							display: inline-block;
+							width: 46px;
+							height: 46px;
+							line-height: 46px;
+							margin-right: 20px;
+						}
+
+						.text {
+							vertical-align: top;
+						}
+					}
+
+					.td2 {
+						width: 200px;
+					}
+
+					.td3 {
+						width: 140px;
+
+						span {
+							background-color: #e1e1e1;
+							border-radius: 4px;
+							padding: 0 20px;
+						}
+					}
+				}
+
+				.tbody {
+					.tr {
+						border-bottom: 1px solid #F0F0F0;
+						padding: 18px 0;
+					}
 				}
 			}
 		}
