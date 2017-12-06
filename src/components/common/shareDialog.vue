ec314b518935d7306d5ec6b646c39abe17582c6e
mirekZhou <mirek.zhou@gmail.com>
2017-12-06 22:01:23 +0800

    share

--- src/components/common/shareDialog.vue
+++ src/components/common/shareDialog.vue
@@ -9,13 +9,13 @@
 					<button>复制</button>
 				</div>
 
-<!-- 				<div class="sharePlatform">
+				<div class="sharePlatform">
 					<label>分享至</label>
 					<i class="sina"></i>
 					<i class="wechat"></i>
 					<i class="qq"></i>
 					<span>让好友帮你夺宝成功</span>
-				</div> -->
+				</div>
 
 				<share :config="config"></share>
 
@@ -48,15 +48,7 @@
 			return {
 				link: '',
 				config: {
-					url                 : window.location.href, // 网址，默认使用 window.location.href
-					source              : '', // 来源（QQ空间会用到）, 默认读取head标签：<meta name="site" content="http://overtrue" />
-					title               : document.title, // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
-					description         : '哈哈哈', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
-					image               : '', // 图片, 默认取网页中第一个img标签
-					sites               : ['qzone', 'qq', 'weibo','wechat', 'douban'], // 启用的站点
-					disabled            : ['google', 'facebook', 'twitter'], // 禁用的站点
-					wechatQrcodeTitle   : '微信扫一扫：分享', // 微信二维码提示文字
-					wechatQrcodeHelper  : '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>'
+					disabled : ['google', 'facebook', 'twitter', 'douban', 'qzone', 'linkedin', 'diandian', 'tencent']
 				}
 			}
 		},
