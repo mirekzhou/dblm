ed81803608571a17aa5903e7d24ecf2e8a8e31ae
mirekZhou <mirek.zhou@gmail.com>
2017-12-13 08:04:38 +0800

    ~~

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -91,6 +91,10 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../data'),
+        to: config.build.assetsRoot + '/data'
       }
     ])
   ]
